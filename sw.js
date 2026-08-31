83407e0c1282f877d1ad10128a9760d3a9ef44b5
tomverin <thomas.verin@sonarsource.com>
2026-08-31 09:02:33 +0200

    Regenerate race PWA with custom start from Guillestre (366.0 km, 549 POIs, start 2026-09-05 04:00)

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_APP = 'c555-app-v1788159162';
+const CACHE_APP = 'c555-app-v1788159749';
 const CACHE_TILES = 'c555-tiles-v1';
 const APP_SHELL = ["./index.html", "./manifest.webmanifest"];
 
