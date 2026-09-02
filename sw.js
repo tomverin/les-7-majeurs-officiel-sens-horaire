528a3d50956b7c7287b22761410a7d90a1f014be
tomverin <thomas.verin@sonarsource.com>
2026-09-02 09:44:15 +0200

    Add standalone roadbook.html with offline PWA support and nav button

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
-const CACHE_APP = 'c555-app-v1788334740';
+const CACHE_APP = 'c555-app-v1788335053';
 const CACHE_TILES = 'c555-tiles-v1';
-const APP_SHELL = ["./index.html", "./manifest.webmanifest", "./coureur.html"];
+const APP_SHELL = ["./index.html", "./manifest.webmanifest", "./coureur.html", "./roadbook.html"];
 
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE_APP).then(c => c.addAll(APP_SHELL)));
